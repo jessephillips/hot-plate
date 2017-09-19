08a98258823271f7663c6ef7f73c9c082c33d1ac
Annihilous <benjaminjnorton@gmail.com>
2017-09-19 19:29:49 -0400

    Create User object.

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170919222757) do
     t.string   "username"
     t.string   "secure_id"
     t.string   "slug"
-    t.boolean  "admin?",                 default: false, null: false
+    t.boolean  "is_admin",               default: false, null: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
